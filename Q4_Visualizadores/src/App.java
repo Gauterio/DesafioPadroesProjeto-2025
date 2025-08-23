857515b1cdb0f0e619f6d18d9224a14a0dc9bf8c
Rosana Schreiner Budant <rosana.budant@edu.pucrs.br>
2025-08-23 15:20:38 +0000

    Arrumando main

--- Q4_Visualizadores/src/App.java
+++ Q4_Visualizadores/src/App.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         var dados = new FonteDeDados();
-        var cm = new VisualizadorDeMedia(dados.getValores());
-        var cs = new VisualizadorDeSomatorio(dados.getValores());
+        var cm = new VisualizadorDeMedia();
+        var cs = new VisualizadorDeSomatorio();
+
+        dados.registarObservador(cm);   
+        dados.registarObservador(cs);
 
         Scanner s = new Scanner(System.in);
         int valor = 0;
+        
         while(true){
             System.out.println("\nEntre um valor positivo maior que zero (0=fim):");
             valor = Integer.parseInt(s.nextLine());
