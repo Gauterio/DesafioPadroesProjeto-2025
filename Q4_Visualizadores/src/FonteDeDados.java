8aab844fe35d39d89999ea7c70e5e635343de5b3
Rosana Schreiner Budant <rosana.budant@edu.pucrs.br>
2025-08-23 15:13:30 +0000

    Montando Observer

--- Q4_Visualizadores/src/FonteDeDados.java
+++ Q4_Visualizadores/src/FonteDeDados.java
@@ -2,16 +2,19 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class FonteDeDados{
+public class FonteDeDados implements Observavel {
     private List<Integer> lst;
+    private List<Observador> observadores;
 
     public FonteDeDados(){
         lst = new LinkedList<>();
+        observadores = new ArrayList<>();
     }
 
     public void add(Integer value){
         if (value < 0) throw new IllegalArgumentException("Valor invalido");
         lst.add(value);
+        notificarObservadores(value);
     }
 
     public int quantidade(){
@@ -20,5 +23,22 @@ public class FonteDeDados{
 
     public List<Integer> getValores(){
         return new ArrayList<>(lst);
+    }
+
+    @Override
+    public void registarObservador(Observador obs) {
+        observadores.add(obs);
+    }
+
+    @Override
+    public void removerObservador(Observador obs) {
+        observadores.remove(obs);
+    }
+
+    @Override
+    public void notificarObservadores(int novoValor) {
+        for(Observador obs : observadores){
+            obs.atualizar(novoValor);
+        }
     }
 }
